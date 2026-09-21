c975dbaa9ca763e12d4d70a0b8d5e8c7c83e5d76
Phetso <phetso.nkosi@umuzi.org>
2026-09-21 17:54:09 +0200

    added theurls for the views for the accounts app

--- TRAVELAPI/accounts/urls.py
+++ TRAVELAPI/accounts/urls.py
@@ -1,3 +1,14 @@
 from django.urls import path
-
-urlpatterns = []
+from .views import (
+    register, login, UserProfileView, PasswordChangeView,
+    password_reset_request, password_reset_confirm,
+)
+app_name = 'accounts'
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('password/change/', PasswordChangeView.as_view(), name='password-change'),
+    path('password/reset/', password_reset_request, name='password-reset'),
+    path('password/reset/confirm/', password_reset_confirm, name='password-reset-confirm'),
+]
