d24ae1ba249c03ce262290dd84a93e3a02e50efe
Phetso <phetso.nkosi@umuzi.org>
2026-09-26 13:54:25 +0200

    added the urls for the itineraries app

--- TRAVELAPI/itineraries/urls.py
+++ TRAVELAPI/itineraries/urls.py
@@ -1,3 +1,4 @@
 from django.urls import path
-
-urlpatterns = []
+from .views import ItineraryListCreateView,ItineraryDetailView,TripCollaborationView,DailyPlanListCreateView,trip_search,generate_trip_report,bulk_update_bookings
+app_name='itineraries'
+urlpatterns=[path('',ItineraryListCreateView.as_view(),name='list-create'),path('search/',trip_search,name='trip-search'),path('bulk-update-bookings/',bulk_update_bookings,name='bulk-update-bookings'),path('<int:trip_id>/',ItineraryDetailView.as_view(),name='detail'),path('<int:trip_id>/report/',generate_trip_report,name='trip-report'),path('<int:trip_id>/collaborators/',TripCollaborationView.as_view(),name='collaborators'),path('<int:trip_id>/days/',DailyPlanListCreateView.as_view(),name='daily-plans')]
