35d924e741b23b18b1cb00177d4c1e2ca553784c
Phetso <phetso.nkosi@umuzi.org>
2026-09-26 12:26:28 +0200

    added the urls for the booking app

--- TRAVELAPI/bookings/urls.py
+++ TRAVELAPI/bookings/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
+from .views import BookingViewSet
+from itineraries.views import bulk_update_bookings
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+router=DefaultRouter(); 
+router.register('bookings',BookingViewSet,basename='booking-local')
+app_name='bookings'
+
+urlpatterns=router.urls+[path('bulk-update/',bulk_update_bookings,name='bulk-update')]
