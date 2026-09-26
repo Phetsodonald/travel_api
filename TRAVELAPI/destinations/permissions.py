b8d8191e58cc89e1a1ef18030cff82a07a01b59d
Phetso <phetso.nkosi@umuzi.org>
2026-09-26 12:54:14 +0200

    added the permissions for the destinations app

---
+++ TRAVELAPI/destinations/permissions.py
@@ -0,0 +1,5 @@
+from rest_framework.permissions import BasePermission
+
+class IsDestinationAdminOrReadOnly(BasePermission):
+    def has_permission(self,request,view): 
+        return request.method in ('GET','HEAD','OPTIONS') or request.user.is_staff
