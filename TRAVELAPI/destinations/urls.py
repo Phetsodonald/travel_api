6962c73fe1d4f967440f9861d1ff6d7bdc508681
Phetso <phetso.nkosi@umuzi.org>
2026-09-26 12:59:20 +0200

    added the views for the destinations app

--- TRAVELAPI/destinations/urls.py
+++ TRAVELAPI/destinations/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from .views import DestinationSearchView,destination_search_preferences
 
-urlpatterns = []
+app_name='destinations'
+urlpatterns=[
+    path('search/',DestinationSearchView.as_view(),name='search'),
+    path('preferences/',destination_search_preferences,name='preferences')
+]
